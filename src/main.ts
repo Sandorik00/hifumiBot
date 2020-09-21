1d02b100b53131918801e39a66320b8eec53e415
Sandorik00 <sasha.kobec00@gmail.com>
2020-09-22 00:01:05 +0300

    fix crash if command is not exist

--- src/main.ts
+++ src/main.ts
@@ -1,69 +1,77 @@
-import * as discord from 'discord.js';
-import {Client, MessageEmbed, MessageAttachment, Message} from 'discord.js';
-import * as fs from 'fs';
+import * as discord from "discord.js";
+import { Client, MessageEmbed, MessageAttachment, Message } from "discord.js";
+import * as fs from "fs";
 
-const configFile: string = fs.readFileSync('./secrets/bot_config.json', 'utf8');
+const configFile: string = fs.readFileSync("./secrets/bot_config.json", "utf8");
 const configs: object = JSON.parse(configFile);
 
-
 export const client = new discord.Client();
-export const prefix: string = configs['prefix'];
-export const ownerID: string = configs['ownerID'];
+export const prefix: string = configs["prefix"];
+export const ownerID: string = configs["ownerID"];
 export const commands: Map<string, Command> = new Map();
 export const dataObj = {};
 
 //clean function
 export function clean(text: string) {
-  if (typeof(text) === "string")
-    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-  else
-      return text;
+  if (typeof text === "string")
+    return text
+      .replace(/`/g, "`" + String.fromCharCode(8203))
+      .replace(/@/g, "@" + String.fromCharCode(8203));
+  else return text;
 }
 
-
 //command handler
-interface Command 
-{
+interface Command {
   name: string;
   run(message?: Message, args?: Array<String>): void;
 }
 
-const commandFiles = fs.readdirSync(__dirname + '/commands').filter(file => file.endsWith('.js'));
-for (let file of commandFiles)
-{
+const commandFiles = fs
+  .readdirSync(__dirname + "/commands")
+  .filter((file) => file.endsWith(".js"));
+for (let file of commandFiles) {
   let command: Command = require(`./commands/${file}`);
   commands.set(command.name, command);
 }
 
 //commandRunner
-export function commandRun(commandName: string, message?: Message, args?: string[]): void
-{
+export function commandRun(
+  commandName: string,
+  message?: Message,
+  args?: string[]
+): void {
   console.log(commandName);
-  if (message && args)
-  {
-    commands.get(commandName).run(message, args);
-  } else if (message)
-  {
-    commands.get(commandName).run(message);
-  } else commands.get(commandName).run();
+  try {
+    if (message && commands.get(commandName) === undefined) {
+      message.channel.send("Нет такой комманды, дурачок.");
+      return;
+    }
+    if (message && args) {
+      commands.get(commandName).run(message, args);
+    } else if (message) {
+      commands.get(commandName).run(message);
+    } else commands.get(commandName).run();
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 //data handler
-const dataFiles = fs.readdirSync('./data').filter(file => file.endsWith('.json'));
-for (let file of dataFiles)
-{
-  let tmpData = fs.readFileSync(`./data/${file}`, 'utf8');
+const dataFiles = fs
+  .readdirSync("./data")
+  .filter((file) => file.endsWith(".json"));
+for (let file of dataFiles) {
+  let tmpData = fs.readFileSync(`./data/${file}`, "utf8");
   let fn = file.replace(/\.[^/.]+$/, "");
   dataObj[fn] = JSON.parse(tmpData);
 }
 
 //event handler
-const eventFiles = fs.readdirSync(__dirname + '/events').filter(file => file.endsWith('.js'));
-for (let file of eventFiles)
-{
-  require(`./events/${file}`)
+const eventFiles = fs
+  .readdirSync(__dirname + "/events")
+  .filter((file) => file.endsWith(".js"));
+for (let file of eventFiles) {
+  require(`./events/${file}`);
 }
 
-
-
-client.login(configs['bot-token']);
+client.login(configs["bot-token"]);
