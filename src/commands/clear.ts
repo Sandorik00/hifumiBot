251d88ae5870808205d4bbfd031d560bb2e491ab
Sandorik00 <sasha.kobec00@gmail.com>
2020-10-15 22:51:43 +0300

    fix 'unknown message' error and make some optimizations in clear command

--- src/commands/clear.ts
+++ src/commands/clear.ts
@@ -22,15 +22,19 @@ module.exports = {
       }
 
       const messages = await message.channel.messages.fetch(options);
-      sum_messages.push(...messages.array());
-      last_id = messages.last().id;
+      sum_messages.push(...messages.values());
+      last_id = sum_messages[sum_messages.length - 1]!!!!!!!!!!!!!.id;
 
       if (args[2]) {
         sum_messages = sum_messages.filter((m) => m.author.username == args[2]);
         console.log(sum_messages);
       }
 
-      if (sum_messages.length >= 1000 || messages.size != 100 || sum_messages.length >= args[1]) {
+      if (
+        sum_messages.length >= 1000 ||
+        messages.size != 100 ||
+        sum_messages.length >= args[1]
+      ) {
         break;
       }
     }
@@ -41,27 +45,30 @@ module.exports = {
 
     let filter = (reaction, user) => user.id === message.author.id;
 
-    let confirmMessage = await message.reply(sum_messages[sum_messages.length - 1]["url"]);
+    let confirmMessage = await message.reply(
+      sum_messages[sum_messages.length - 1]["url"]
+    );
 
-    await confirmMessage.react('✅');
-    await confirmMessage.react('❌');
+    await confirmMessage.react("✅");
+    await confirmMessage.react("❌");
 
-    let collector = confirmMessage.createReactionCollector(filter, { time: 120000, dispose: true });
-    let action = async reaction => {
+    let collector = confirmMessage.createReactionCollector(filter, {
+      time: 120000,
+      dispose: true,
+    });
+    let action = async (reaction) => {
       collector.stop();
-      if(reaction.emoji.name == '✅') {
+      if (reaction.emoji.name == "✅") {
         message.channel.bulkDelete(sum_messages);
+      } else {
+        await message.delete();
       }
+    };
 
-      await confirmMessage.delete();
-    }
+    collector.on("collect", action);
 
-    collector.on('collect', action);
-
-    collector.on('end', async collected => {
-      await confirmMessage.delete();
-  })
-
-    await message.delete();
+    collector.on("end", async (collected) => {
+        await confirmMessage.delete();
+    });
   },
 };
