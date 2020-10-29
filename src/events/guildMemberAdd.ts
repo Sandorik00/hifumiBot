5c6efb5f44324a794208d685e383685d683c0785
Sandorik00 <sasha.kobec00@gmail.com>
2020-10-29 23:11:56 +0300

    special greeting for Paradox

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -8,13 +8,19 @@ import
     commandRun
 } from '../main';
 
+const forParadox = ["Если ещё раз выйдешь то бан!!", "Ты совсем дурачок или как?", "Слушай, ну хватит уже...", "Даже если ты не Парадокс, то всё равно бан.", "Sandorik, да вломи ты ему наконец!!"];
+
 client.on('guildMemberAdd', async (member: GuildMember) =>
 {
     const HelloChannel: GuildChannel = member.guild.channels.cache.find(ch => ch.id === '674609296050487306');
     if (!HelloChannel) return;
     //hardcoded now but will be rewrited later
 
-    let InviteStats = await util.getReferalInvites(member.guild);
+    if(member.id === "641366682761166860") {
+        member.roles.add("751923183384264725");
+        (HelloChannel as TextChannel).send(forParadox[Math.floor(Math.random() * forParadox.length)]);
+    } else {
+        let InviteStats = await util.getReferalInvites(member.guild);
     let oldStats = await util.getInvitesData();
     let from: string = await util.fromCheck(InviteStats, oldStats);
     let memData: util.MemberData = new util.MemberData(member, from, InviteStats);
@@ -31,4 +37,5 @@ client.on('guildMemberAdd', async (member: GuildMember) =>
     
 
     await fr.writeToCollection('users', memData.toJSON());
+    }
 })
