330cfbf4d410fbaf8e16a4b6b5499ebc88423183
Sandorik00 <sasha.kobec00@gmail.com>
2020-11-07 18:23:04 +0300

    refactor "ava" command

--- src/commands/ava.ts
+++ src/commands/ava.ts
@@ -1,90 +1,70 @@
 import { client } from "../main";
-import { User, ImageURLOptions, Message } from "discord.js";
+import {
+  User,
+  ImageURLOptions,
+  Message,
+  Collection,
+  GuildMember,
+  Guild,
+} from "discord.js";
 import fetch from "node-fetch";
 
 module.exports = {
   name: "ava",
-  async run(message, args) {
-    let imageOptions: ImageURLOptions = { format: "png", size: 4096 };
-    let allInOneArgs: String = args.join(" ");
+  async run(message: Message, args: Array<string>) {
+    let imageOptions: ImageURLOptions & { dynamic?: boolean } = {
+      format: "png",
+      size: 4096,
+      dynamic: true,
+    };
+    let allInOneArgs: string = args.join(" ");
     let generalArg = allInOneArgs.substring(args[0].length + 1);
-    let avatar: String;
+    let avatar: string;
+    let user: User;
     if (!args[1]) {
-      avatar = message.author.avatarURL(imageOptions);
+      user = message.author;
     } else {
       let arg = String(args[1]);
       try {
         switch (true) {
           case /<@!\d+>/.test(arg):
-            let user = (await client.users.fetch(
-              args[1].match(/<@!(\d*)>/)[1]
-            )) as User;
-            avatar = user.avatarURL(imageOptions);
-            if (avatar == undefined) {
-              avatar = user.defaultAvatarURL;
-            }
+            user = (await client.users.fetch(args[1].match(/<@!(\d*)>/)[1])) as User;
             break;
 
           case /\d+/.test(arg):
-            let userById = (await client.users.fetch(arg, false, true)) as User;
-            avatar = userById.avatarURL(imageOptions);
-            if (avatar == undefined) {
-              avatar = userById.defaultAvatarURL;
-            }
+            user = (await client.users.fetch(arg, false, true)) as User;
             break;
 
           default:
-            let allMembers = await message.guild.members.fetch();
-            let userByNick =
+            let allMembers: Collection<string, GuildMember> = await message.guild.members.fetch();
+            let tempMem: GuildMember;
+            tempMem =
               allMembers.find(
                 (mem) =>
                   mem.user.username.toLowerCase() === generalArg.toLowerCase()
               ) ?? null;
-            if (userByNick != null) {
-              if (userByNick.hasOwnProperty("user")) {
-                userByNick = userByNick.user;
-              }
-            }
-            if (!userByNick) {
-              userByNick = allMembers.find((mem) => mem.nickname === generalArg)
-                .user;
-            }
 
-            if (!userByNick) {
-              message.channel.send("Не знаю никого с таким именем.");
-            } else {
-              avatar = userByNick.avatarURL(imageOptions);
-              if (avatar == undefined) {
-                avatar = userByNick.defaultAvatarURL;
-              }
-            }
+            user =
+              user != null
+                ? tempMem.user
+                : allMembers.find((mem) => mem.nickname === generalArg).user;
+
             break;
         }
       } catch (e) {
         console.log(e);
-        message.channel.send(
-          "Ой, похоже тут какая-то ошибка. Попробуй ещё раз."
-        );
+        message.channel.send(e);
       }
     }
-    message.channel.send(avatar);
+
+    if (!user) {
+      message.channel.send("Никого такого не знаю.");
+    } else {
+      avatar =
+        user.avatarURL(imageOptions) !== null
+          ? user.avatarURL(imageOptions)
+          : user.defaultAvatarURL;
+      message.channel.send(avatar);
+    }
   },
 };
-
-/* alternative node-fetch by id implementation with not actual token of course
-let userById;
-            const url = `https://discordapp.com/api/users/${arg}`;
-            const headers = {
-              "Content-Type": "application/json",
-              "Authorization": "Bot NjUyODQxNTg1NTkwOTI3Mzkw.XeuUGw.cjHtR4VVVZER6uQlmtZKKzukYqY",
-            };
-
-             await fetch(url, { method: "GET", headers: headers })
-              .then((res) => {
-                return res.json();
-              })
-              .then((json) => {
-                userById = json;
-                console.log(json);
-              });
-              avatar = `https://cdn.discordapp.com/avatars/${userById.id}/${userById.avatar}.png?size=4096`;  */
